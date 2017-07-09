905842589c3ea2af69cfd27b7faf604c53b1c280
Shane Qi <qizengtai@gmail.com>
2017-07-09 16:10:30 -0500

    Added xcodeproj.

---
+++ SwipeSelectingCollectionView/SwipeSelectingCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  SwipeSelectingCollectionView.h
+//  SwipeSelectingCollectionView
+//
+//  Created by Shane Qi on 7/9/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeSelectingCollectionView.
+FOUNDATION_EXPORT double SwipeSelectingCollectionViewVersionNumber;
+
+//! Project version string for SwipeSelectingCollectionView.
+FOUNDATION_EXPORT const unsigned char SwipeSelectingCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeSelectingCollectionView/PublicHeader.h>
+
+
